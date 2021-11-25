688d8597af53e1b4a52a649b4a651cb6dcdf1382
skang404 <skang404@gmail.com>
2021-11-24 17:31:46 -0800

    cleaned repo

--- scripts/1_fixedTermStaking-truffle-port.js
+++
@@ -1,9 +0,0 @@
-const Staking = artifacts.require("lunaFund");
-const Erc20 = artifacts.require('Lunatoken');
-
-module.exports = async (deployer) => {
-  const erc20 = await Erc20.new();
-  Erc20.setAsDeployed(erc20);
-  const staking = await Staking.new();
-  Staking.setAsDeployed(staking);
-};
