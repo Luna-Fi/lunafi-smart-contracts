df7ba19785f8847e5c21c06d43a98b4f952e9723
cvkonduru <cvkonduru@gmail.com>
2021-12-10 13:31:24 -0500

    Removed MockTOkens from diamond

--- contracts/repositories/ERC20StorageRepository.sol
+++ contracts/repositories/ERC20StorageRepository.sol
@@ -3,25 +3,10 @@ pragma solidity ^0.8.10;
 
 library TokenStorageContract {           
 
-    bytes32 internal constant USDCTESTTOKEN_STORAGE_POSITION = keccak256("USDCToken.token.diamond.ERC20Token");
-    bytes32 internal constant WETHTESTTOKEN_STORAGE_POSITION = keccak256("WETHToken.token.diamond.ERC20Token");
-    bytes32 internal constant WBTCTESTTOKEN_STORAGE_POSITION = keccak256("WBTCToken.token.diamond.ERC20Token");
-
     bytes32 internal constant USDCCLAIMTOKEN_STORAGE_POSITION = keccak256("USDCClaimToken.token.diamond.ERC20Token");
     bytes32 internal constant WBTCCLAIMTOKEN_STORAGE_POSITION = keccak256("WBTCClaimToken.token.diamond.ERC20Token");
     bytes32 internal constant WETHCLAIMTOKEN_STORAGE_POSITION = keccak256("WETHClaimToken.token.diamond.ERC20Token");
     
-    struct ERC20TokenStorage {  
-        uint8  decimals;
-        address  owner; 
-        string  name;
-        string  symbol;
-        uint256  initialSupply;
-        uint256  _totalSupply; 
-        mapping(address => uint) balances;      
-        mapping(address => mapping(address => uint)) approved; 
-        mapping(address => mapping(address => uint))  allowed;      
-    }
 
     struct ClaimTokenStorage {  
         uint8  decimals;
@@ -35,22 +20,6 @@ library TokenStorageContract {
         mapping(address => bool)  admins;
              
     }
-
-
-    function usdcTestTokenStorage() internal pure returns(ERC20TokenStorage storage ds) {
-        bytes32 position = USDCTESTTOKEN_STORAGE_POSITION;
-        assembly { ds.slot := position }
-    }  
-
-    function wethTestTokenStorage() internal pure returns(ERC20TokenStorage storage ds) {
-        bytes32 position = WETHTESTTOKEN_STORAGE_POSITION;
-        assembly { ds.slot := position }
-    }  
-
-    function wbtcTestTokenStorage() internal pure returns(ERC20TokenStorage storage ds) {
-        bytes32 position = WBTCTESTTOKEN_STORAGE_POSITION;
-        assembly { ds.slot := position }
-    } 
 
     function usdcClaimTokenStorage() internal pure returns(ClaimTokenStorage storage ds) {
         bytes32 position = USDCCLAIMTOKEN_STORAGE_POSITION;
---
+++ scripts/deployMockTokens.js
@@ -0,0 +1,23 @@
+
+const main = async () => {
+  const mockUSDCToken = await ethers.getContractFactory("mockUSDCToken");
+  const mockWETHToken = await ethers.getContractFactory("mockWETHToken");
+  const mockWBTCToken = await ethers.getContractFactory("mockWBTCToken");
+
+  const USDCToken = await mockUSDCToken.deploy();
+  const WETHToken = await mockWETHToken.deploy();
+  const WBTCToken = await mockWBTCToken.deploy();
+
+  console.log("MockUSDCToken deployed to : ", USDCToken.address);
+
+
+
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
