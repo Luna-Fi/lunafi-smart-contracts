514b75d6133e32b850e8a5454cdadf6339ae6caf
cvkonduru <cvkonduru@gmail.com>
2021-12-14 08:35:04 -0500

    added initial packages for hardhat-deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.10",
