63fe6f614d71a56f223224d6753d6945516bca3b
cvkonduru <cvkonduru@gmail.com>
2021-12-15 09:07:05 -0500

    Deploy script added to deploy to Ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,22 +4,41 @@
  */
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-deploy");
+require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 
-module.exports = {
+const config = {
   solidity: "0.8.10",
   networks : {
+    localhost: {
+      url: "http://127.0.0.1:8545/"
+    },
     ropsten: {
-      url : "https://ropsten.infura.io/v3/06ad6936699f4bd2887f8d4db7e2b613",
+      url: `${process.env.NODE_URI_ROPSTEN}`,
       chainId: 3,
+      live: true,
+      tags: ["staging"],
+      saveDeployments: true,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `${process.env.NODE_URI_MAINNET}`,
+      chainId: 3,
+      live: true,
+      tags: ["production"],
+      saveDeployments: true,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
     }
   },
   namedAccounts: {
     deployer: {
         default: 0, 
-        3:0,
+        localhost: `${process.env.DEPLOYER_ACCOUNT_ADDRESS}`,
+        ropsten: `${process.env.DEPLOYER_ACCOUNT_ADDRESS}`
     }     
   }
 };
+
+module.exports = config;
 
 
