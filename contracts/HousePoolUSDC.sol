b60bca84e2718619b0b3618d3344651cc937b0ed
nucodeio <nucoder@nucode.io>
2022-03-09 16:11:22 -0500

    stripped down usdc house pool;

--- contracts/HousePoolUSDC.sol
+++ contracts/HousePoolUSDC.sol
@@ -6,84 +6,29 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "contracts/interfaces/IclaimToken.sol";
 import "hardhat/console.sol";
 
 contract HousePoolUSDC is
     ReentrancyGuardUpgradeable,
-    AccessControl,
-    EIP712Upgradeable
+    AccessControl
 {
-    using SafeERC20 for IERC20;
-    struct ValuesOfInterest {
-        int256 expectedValue;
-        int256 maxExposure;
-        uint256 deadline;
-        address signer;
-    }
+    // DO NOT CHANGE THE NAME, TYPE OR ORDER OF EXISITING VARIABLES BELOW
 
-    uint256 bettingStakes;
+    using SafeERC20 for IERC20;
     uint256 liquidity;
-    uint256 treasuryAmount = 0; // Initialized this to suffice uninitialized-state-variables
-    int256 tvl;
     IERC20 token;
     claimTokenInterface claimToken;
-    ValuesOfInterest public voi;
 
     uint256 constant MAX_PRECISION = 18;
     uint256 constant PRECISION_DIFFERENCE = 12;
     uint256 lpTokenPrice;
     uint256 lpTokenWithdrawlPrice;
 
-    bytes32 public constant DATA_PROVIDER_ORACLE =
-        keccak256("DATA_PROVIDER_ORACLE");
-    bytes32 public constant HOUSE_POOL_DATA_PROVIDER =
-        keccak256("HOUSEPOOL_DATA_PROVIDER");
-
-    mapping(address => uint256) private nonces;
     mapping(address => uint256) private deposits;
 
-    modifier onlyValid(ValuesOfInterest memory data, bytes memory signature) {
-        bytes32 digest = _hashTypedDataV4(
-            keccak256(
-                abi.encode(
-                    keccak256(
-                        "VoI(address signer,int256 expectedValue,uint256 maxExposure,uint256 nonce,uint256 deadline)"
-                    ),
-                    data.signer,
-                    data.expectedValue,
-                    data.maxExposure,
-                    nonces[data.signer],
-                    data.deadline
-                )
-            )
-        );
-        require(
-            SignatureChecker.isValidSignatureNow(
-                data.signer,
-                digest,
-                signature
-            ),
-            "HousePoolUSDC: invalid signature"
-        );
-
-        require(data.signer != address(0), "HousePoolUSDC: invalid signer");
-
-        require(
-            hasRole(DATA_PROVIDER_ORACLE, data.signer),
-            "HousePoolUSDC: unauthorised signer"
-        );
-
-        require(
-            block.number < data.deadline,
-            "HousePoolUSDC: signed transaction expired"
-        );
-
-        //nonces[data.signer]++;
-        _;
-    }
+    // DO NOT CHANGE THE NAME, TYPE OR ORDER OF EXISITING VARIABLES ABOVE
 
     // -- Init --
     function initialize(
@@ -101,33 +46,6 @@ contract HousePoolUSDC is
         lpTokenWithdrawlPrice = 100 * 10**MAX_PRECISION;
     }
 
-    // -- Authorised Functions --
-    function setVOI(bytes memory sig_, ValuesOfInterest memory voi_)
-        external
-        onlyValid(voi_, sig_)
-        onlyRole(HOUSE_POOL_DATA_PROVIDER)
-    {
-        _setVoi(voi_);
-    }
-
-    function deposit(
-        uint256 amountUSDC,
-        bytes memory approval,
-        ValuesOfInterest memory approvedValues
-    ) external onlyValid(approvedValues, approval) {
-        _setVoi(approvedValues);
-        _deposit(amountUSDC);
-    }
-
-    function withdraw(
-        uint256 amountUSDC,
-        bytes memory approval,
-        ValuesOfInterest memory approvedValues
-    ) external onlyValid(approvedValues, approval) {
-        _setVoi(approvedValues);
-        _withdraw(amountUSDC);
-    }
-
     // -- External Functions
     function deposit_(uint256 usdcMicro) external {
         _deposit(usdcMicro);
@@ -142,24 +60,8 @@ contract HousePoolUSDC is
         return lpTokenPrice;
     }
 
-    function getTreasuryAmount() external view returns (uint256) {
-        return treasuryAmount;
-    }
-
     function getTokenWithdrawlPrice() external view returns (uint256) {
         return lpTokenWithdrawlPrice;
-    }
-
-    function getTVLofPool() external view returns (int256) {
-        return tvl;
-    }
-
-    function getEV() external view returns (int256) {
-        return voi.expectedValue;
-    }
-
-    function getMaxExposure() external view returns (int256) {
-        return voi.maxExposure;
     }
 
     function getLiquidityStatus() external view returns (uint256) {
@@ -171,49 +73,6 @@ contract HousePoolUSDC is
     }
 
     // -- Internal Functions --
-    function _setTokenPrice() internal {
-        if (claimToken.totalSupply() != 0) {
-            lpTokenPrice =
-                (uint256(tvl) * 10**MAX_PRECISION) /
-                claimToken.totalSupply();
-        }
-    }
-
-    function _setTokenWithdrawlPrice() internal {
-        if (claimToken.totalSupply() != 0) {
-            lpTokenWithdrawlPrice =
-                (liquidity * 10**MAX_PRECISION) /
-                claimToken.totalSupply();
-        }
-    }
-
-    function _updateTVL(int256 expectedValue) internal {
-        if (voi.expectedValue == 0) {
-            tvl += expectedValue;
-        } else {
-            tvl -= voi.expectedValue;
-            tvl += expectedValue;
-        }
-    }
-
-    function _setVoi(ValuesOfInterest memory _voi) internal {
-        if (_voi.expectedValue != voi.expectedValue) {
-            _setEV(_voi.expectedValue);
-        }
-        if (_voi.maxExposure != voi.maxExposure) {
-            _setME(_voi.maxExposure);
-        }
-    }
-
-    function _setME(int256 exposure) internal {
-        voi.maxExposure = exposure;
-    }
-
-    function _setEV(int256 newEV) internal {
-        _updateTVL(newEV);
-        voi.expectedValue = newEV;
-        _setTokenPrice();
-    }
 
     function _deposit(uint256 amount) internal nonReentrant {
         require(
@@ -221,15 +80,12 @@ contract HousePoolUSDC is
             "USDCHousePool: Check the Balance"
         );
         liquidity += amount * 10**PRECISION_DIFFERENCE;
-        tvl += int256(amount * 10**PRECISION_DIFFERENCE);
         deposits[msg.sender] += amount * 10**PRECISION_DIFFERENCE;
         token.safeTransferFrom(msg.sender, address(this), amount);
         uint256 LPTokensToMint = (amount *
             10**PRECISION_DIFFERENCE *
             10**MAX_PRECISION) / lpTokenPrice;
         claimToken.mint(msg.sender, LPTokensToMint);
-        _setTokenPrice();
-        _setTokenWithdrawlPrice();
     }
 
     function _withdraw(uint256 amount) internal nonReentrant {
@@ -239,18 +95,15 @@ contract HousePoolUSDC is
                 (claimToken.balanceOf(msg.sender) / 10**MAX_PRECISION) *
                     lpTokenWithdrawlPrice &&
                 int256(amount) * int256(10**PRECISION_DIFFERENCE) <=
-                int256(liquidity) - voi.maxExposure,
+                int256(liquidity),
             "USDCHousePool : can't withdraw"
         );
         uint256 LPTokensToBurn = (amount *
             10**PRECISION_DIFFERENCE *
             10**MAX_PRECISION) / (lpTokenWithdrawlPrice);
         liquidity -= amount * 10**PRECISION_DIFFERENCE;
-        tvl -= int256(amount) * int256(10**PRECISION_DIFFERENCE);
         deposits[msg.sender] -= amount * 10**PRECISION_DIFFERENCE;
         token.safeTransfer(msg.sender, amount);
         claimToken.burn(msg.sender, LPTokensToBurn);
-        _setTokenWithdrawlPrice();
-        _setTokenPrice();
     }
 }
