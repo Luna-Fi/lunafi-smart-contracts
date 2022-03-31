aac4ff45ebcd42839c8fe0a6f3ea9207d9645db3
Chaitanya Varma Konduru <cvkonduru@gmail.com>
2022-03-31 14:07:52 -0400

    LFI Token tests

--- test/ClaimToken/ClaimToken.test.js
+++ test/ClaimToken/ClaimToken.test.js
@@ -73,7 +73,7 @@ describe("Claim Token", () => {
 
     it("Should not allow the non owner to mint tokens", async () => {
         const [owner,user1] = await ethers.getSigners();
-        const tokens = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
+        const tokens = ethers.utils.formatUnits(returnBigNumber(20 * 10**18),0)
         await claimToken.addAdmin(owner.address)
         await expect(claimToken.connect(user1).mint(user1.address,tokens)).to.be.revertedWith("")
         
@@ -81,16 +81,56 @@ describe("Claim Token", () => {
 
     it("Should allow the admin to burn tokens", async () => {
         const [owner,user1] = await ethers.getSigners();
-        const tokensToMint = ethers.utils.formatUnits(returnBigNumber(20 * 10**18),0)
         const tokensToBurn = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
         await claimToken.addAdmin(owner.address)
-        await claimToken.mint(user1.address,tokensToMint)
         const balanceOfUserBeforeBurn = await claimToken.balanceOf(user1.address);
         await claimToken.burn(user1.address,tokensToBurn)
-        const BalanceAfterBurn =  ethers.utils.formatUnits(await claimToken.balanceOf(user1.address),0);
-        expect(BalanceAfterBurn).to.equal(tokensToMint - tokensToBurn)
+        const BalanceAfterBurn =  await claimToken.balanceOf(user1.address);
+        expect(BalanceAfterBurn).to.equal("0")
+    })
+
+    it("Should not allow the non admin to burn tokens", async () => {
+        const [owner,user1] = await ethers.getSigners();
+        const tokensToBurn = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
+        await claimToken.addAdmin(owner.address)
+        const balanceOfUserBeforeBurn = await claimToken.balanceOf(user1.address);
+        await expect(claimToken.connect(user1).burn(user1.address,tokensToBurn)).to.be.revertedWith("")
+        
+    })
+
+    it("Should allow the any user to transfer tokens to another user", async () => {
+        const [owner,user1,user2] = await ethers.getSigners();
+        const tokensToMint = ethers.utils.formatUnits(returnBigNumber(40 * 10**18),0)
+        const tokensToTransfer = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
+        await claimToken.addAdmin(owner.address)
+        const balanceOfUser1 = await claimToken.balanceOf(user1.address)
+        await claimToken.mint(user1.address,tokensToMint)
+        const balanceOfUser1AfterMint = await claimToken.balanceOf(user1.address)
+        await claimToken.connect(user1).transfer(user2.address,tokensToTransfer)
+        const balanceOfUser2 = await claimToken.balanceOf(user2.address)
+        expect(balanceOfUser2).to.equal(tokensToTransfer)
 
     })
 
+    it("Should allow the user to return totalSupply", async() => {
+        const totalSupply = await claimToken.totalSupply()
+        expect(totalSupply).to.equal(ethers.utils.formatUnits(returnBigNumber(40 * 10**18),0));
+    })
 
+    it("Should return the balance of the given user", async() => {
+        const [owner,user1,user2] = await ethers.getSigners();
+        const balance = await claimToken.balanceOf(user1.address);
+        expect(balance).to.equal(ethers.utils.formatUnits(returnBigNumber(30 * 10**18),0));
+    })
+
+    it("Should allow the user2 to spend user1s tokens", async() => {
+        const [owner,user1,user2] = await ethers.getSigners();
+        const tokensToUse = ethers.utils.formatUnits(returnBigNumber(40 * 10**18),0)
+        await claimToken.addAdmin(owner.address)
+        await claimToken.mint(user1.address,tokensToUse)
+        await claimToken.connect(user1).approve(user2.address,tokensToUse);
+        await claimToken.connect(user2).transferFrom(user1.address,user2.address,tokensToUse)
+        const balanceOfUser2 = await claimToken.balanceOf(user2.address);
+        expect(balanceOfUser2).to.equal(ethers.utils.formatUnits(returnBigNumber(50 * 10**18),0));
+    })
 })
