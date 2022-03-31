08de7861f0bc9a2e316a3ea4d7a1a1996e215e77
Chaitanya Varma Konduru <cvkonduru@gmail.com>
2022-03-31 09:57:38 -0400

    WIP ClaimToken Test cases

--- test/ClaimToken/ClaimToken.test.js
+++ test/ClaimToken/ClaimToken.test.js
@@ -44,7 +44,7 @@ describe("Claim Token", () => {
         expect(isAdmin).equal(false);
     })
 
-    it("Should now allow the non owner to remove admin", async() => {
+    it("Should not allow the non owner to remove admin", async() => {
         const [owner,user1,user2] = await ethers.getSigners();
         //await claimToken.connect(user1).addAdmin(user2.address)
         await expect(claimToken.connect(user1).removeAdmin(user2.address)).to.be.revertedWith("")
@@ -55,14 +55,41 @@ describe("Claim Token", () => {
         await expect(claimToken.connect(user1).addAdmin(user1.address)).to.be.revertedWith("")
     })
 
-    it("Should allow the admin to mint tokens and should not exceed maxCap", async () => {
+    it("Should allow the admin to mint tokens", async () => {
         const [owner, user1] = await ethers.getSigners();
         const tokens = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
         await claimToken.addAdmin(owner.address)
         await claimToken.mint(user1.address,tokens)
         const totalSupplyAfterMint = await claimToken.totalSupply();
-        expect(totalSupplyAfterMint ).to.be.equal(tokens);
+        expect(totalSupplyAfterMint ).to.be.equal(tokens);  
+    })
+
+    it("Should allow the admin to mint the tokens, but cannot exceed maxCap", async() => {
+        const [owner, user1] = await ethers.getSigners();
+        const tokens = ethers.utils.formatUnits(returnBigNumber(1000000001 * 10**18),0)
+        await claimToken.addAdmin(owner.address)
+        await expect(claimToken.mint(user1.address,tokens)).to.be.revertedWith("")
+    })
+
+    it("Should not allow the non owner to mint tokens", async () => {
+        const [owner,user1] = await ethers.getSigners();
+        const tokens = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
+        await claimToken.addAdmin(owner.address)
+        await expect(claimToken.connect(user1).mint(user1.address,tokens)).to.be.revertedWith("")
         
+    })
+
+    it("Should allow the admin to burn tokens", async () => {
+        const [owner,user1] = await ethers.getSigners();
+        const tokensToMint = ethers.utils.formatUnits(returnBigNumber(20 * 10**18),0)
+        const tokensToBurn = ethers.utils.formatUnits(returnBigNumber(10 * 10**18),0)
+        await claimToken.addAdmin(owner.address)
+        await claimToken.mint(user1.address,tokensToMint)
+        const balanceOfUserBeforeBurn = await claimToken.balanceOf(user1.address);
+        await claimToken.burn(user1.address,tokensToBurn)
+        const BalanceAfterBurn =  ethers.utils.formatUnits(await claimToken.balanceOf(user1.address),0);
+        expect(BalanceAfterBurn).to.equal(tokensToMint - tokensToBurn)
+
     })
 
 
