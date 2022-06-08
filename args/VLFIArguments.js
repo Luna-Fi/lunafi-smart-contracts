02585d2bbf0763ab9dc3fe1b98ae06c4d5231c98
nucodeio <nucoder@nucode.io>
2022-06-08 07:10:07 -0400

    post vlfi deployment config

--- args/VLFIArguments.js
+++ args/VLFIArguments.js
@@ -1,15 +1,16 @@
-const { BigNumber} = require("ethers");
+const { BigNumber } = require("ethers");
 
 const returnBigNumber = (number) => {
     number = number.toString(16)
     return BigNumber.from("0x" + number);
 }
 
-const rewardsPerSecond = ethers.utils.formatUnits(returnBigNumber(1 * 10 **18),0);
+const rewardsPerSecond = ethers.BigNumber.from('86805555555555600')//ethers.utils.BigNumber.from('86805555555555600');
+
 module.exports = [
     "VLFI",
-    "VLFI",
-    "0x9c4c940205cF972e0B5742c17B3B9a3eAAF87a47",
+    "vLFI",
+    "0xCa7BF3C514d412AC12D10Eff302301A81153F557",
     864000,
     172800,
     rewardsPerSecond
--- scripts/VLFI/deploy_VLFI.js
+++ scripts/VLFI/deploy_VLFI.js
@@ -1,25 +1,26 @@
 const { BigNumber } = require("ethers");
 const returnBigNumber = (number) => {
-    number = number.toString(16)
-    return BigNumber.from("0x" + number);
+  number = number.toString(16)
+  return BigNumber.from("0x" + number);
 }
 async function main() {
 
-    const name = "VLFI"
-    const symbol = "VLFI"
-    const stakedToken = "0x9c4c940205cF972e0B5742c17B3B9a3eAAF87a47"
-    const cooldownSeconds = 864000 // Use this in production. 864000 is equal to 10 days
-    const unstakeWindow = 172800 // use this in production. 172800 is equal to 2 days.
-    const rewardPerSecond = ethers.utils.formatUnits(returnBigNumber(1 * 10**18),0)
-    const VLFI = await ethers.getContractFactory("VLFI");
-    console.log("Deploying VLFI...");
-    const vlfi = await upgrades.deployProxy(VLFI,[name,symbol,stakedToken,cooldownSeconds,unstakeWindow,rewardPerSecond],{initializer: 'initialize'});
-    console.log("VLFI Deployed to :", vlfi.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+  const name = "VLFI"
+  const symbol = "vLFI"
+  const stakedToken = "0xCa7BF3C514d412AC12D10Eff302301A81153F557"
+  const cooldownSeconds = 864000 // Use this in production. 864000 is equal to 10 days
+  const unstakeWindow = 172800 // use this in production. 172800 is equal to 2 days.
+  const rewardPerSecond = BigNumber.from('86805555555555600')
+  console.log({ rewardPerSecond, rewardPerSecond: rewardPerSecond.toString() })
+  const VLFI = await ethers.getContractFactory("VLFI");
+  console.log("Deploying VLFI...");
+  const vlfi = await upgrades.deployProxy(VLFI, [name, symbol, stakedToken, cooldownSeconds, unstakeWindow, rewardPerSecond], { initializer: 'initialize' });
+  console.log("VLFI Deployed to :", vlfi.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
